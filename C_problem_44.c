ff68cbb867d01b09a0ee79cea21dc41e42b67276
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-09-20 01:12:18 +0530

    Add more patterns

---
+++ C_problem_44.c
@@ -0,0 +1,28 @@
+/* WAP to print some more patterns */
+#include <stdio.h>
+
+int main () {
+    int n = 10;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++){
+            printf("%c", ' ');
+        }
+        for (int k = 1; k <= i; k++){
+            printf("%c", '*');
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 0; j <= i; j++){
+            printf("%c", ' ');
+        }
+        for (int k = 1; k <= n - i + 1; k++){
+            printf("%c", '*');
+        }
+        printf("\n");
+    }
+}
